17a41c0cfa89144e98bf7429c1a0b52620fd186e
Tess Lameyer <tjlameyer@gmail.com>
2020-01-29 14:05:28 -0800

    added word property to form inputs

--- src/components/madlib/Form.js
+++ src/components/madlib/Form.js
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types';
 const Form = ({ onSubmit, val0, val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11 }) => (
 
   <form onSubmit={onSubmit}>
-    <input type="text" value={val0} placeholder="adjective" />
-    <input type="text" value={val1} placeholder="noun" />
-    <input type="text" value={val2} placeholder="verb, past tense" />
-    <input type="text" value={val3} placeholder="adverb" />
-    <input type="text" value={val4} placeholder="adjective" />
-    <input type="text" value={val5} placeholder="noun" />
-    <input type="text" value={val6} placeholder="noun" />
-    <input type="text" value={val7} placeholder="adjective" />
-    <input type="text" value={val8} placeholder="verb" />
-    <input type="text" value={val9} placeholder="adverb" />
-    <input type="text" value={val10}placeholder="verb, past tense" />
-    <input type="text" value={val11} placeholder="adjective" />
+    <input type="text" name='word0' value={val0} placeholder="adjective" />
+    <input type="text" name='word1' value={val1} placeholder="noun" />
+    <input type="text" name='word2' value={val2} placeholder="verb, past tense" />
+    <input type="text" name='word3' value={val3} placeholder="adverb" />
+    <input type="text" name='word4' value={val4} placeholder="adjective" />
+    <input type="text" name='word5' value={val5} placeholder="noun" />
+    <input type="text" name='word6' value={val6} placeholder="noun" />
+    <input type="text" name='word7' value={val7} placeholder="adjective" />
+    <input type="text" name='word8' value={val8} placeholder="verb" />
+    <input type="text" name='word9' value={val9} placeholder="adverb" />
+    <input type="text" name='word10' value={val10}placeholder="verb, past tense" />
+    <input type="text" name='word11' value={val11} placeholder="adjective" />
     <button>Create Madlib</button>
   </form>
 );
--- src/containers/Madlib.js
+++ src/containers/Madlib.js
@@ -4,11 +4,26 @@ import Result from '../components/madlib/Result';
 
 export default class Madlib extends Component{
   state = {
-    showResult: false
+    showResult: false, 
+    words: [],
+    val0: '',
+    val1: '', 
+    val2: '',  
+    val3: '', 
+    val4: '', 
+    val5: '', 
+    val6: '', 
+    val7: '', 
+    val8: '', 
+    val9: '', 
+    val10: '', 
+    val11: '' 
   }
 
   toggleResult = () =>
     this.setState(state => ({ ...state, showResult: !state.showResult }));
+
+  packageWords = ({ target }) => this.setState({ words: [...target.value] });
 
   handleSubmit = event => {
     event.preventDefault();
