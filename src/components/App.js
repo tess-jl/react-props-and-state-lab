033b2c431a1ad47ece7a33b6608d58711b9b4711
Tess Lameyer <tjlameyer@gmail.com>
2020-01-29 13:26:15 -0800

    Header and Footer are present

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Madlib from '../containers/Madlib';
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+
 
 export default function App() {
-  return <Madlib />;
+  return (
+    <>
+      <Header /> 
+      <Madlib /> 
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <>
+    <p>footer</p>
+  </>
+);
+
+export default Footer; 
